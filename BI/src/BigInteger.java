8732568a528ce689f64bae963e3488b928b486d6
hoonga <etaphirho@snu.ac.kr>
2015-03-10 15:58:14 +0900

    first assignment

---
+++ BI/src/BigInteger.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class BigInteger{
+  public static void main(String args[]){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    while (True){
+      try{
+        String input = br.readLine();
+        if(input.compareTo("quit")==0)
+      }
+    }
+  }
+}
